ea699977fcaed1241488ade88c0f3633da430908
ilia <38052676+iliaKulakov@users.noreply.github.com>
2018-06-02 22:37:55 +0300

    small fixes

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -13,17 +13,16 @@ public class SeleniumTest {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-//        System.setProperty("webdriver.chrome.driver", "/home/rmuhamedgaliev/Downloads/chromedriver"); // path of chromedriver
-       System.setProperty("webdriver.chrome.driver", "C:/Users/Angron/Downloads/chromedriver.exe"); // path of chromedriver
-
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Angron/Downloads/chromedriver.exe"); // path of chromedriver
         WebDriver driver = new ChromeDriver();
         driver.get("http://qa.sedmax.ru/");
         driver.findElement(By.id("LoginForm_account")).sendKeys("test");
         driver.findElement(By.id("LoginForm_password")).sendKeys("test");
-
         WebElement enterButton= driver.findElement(By.xpath("//*[@id=\"login-form\"]/div[4]/button"));
         enterButton.click();
-        //WebElement enterButton1 = driver.findElement(By.className("sub-menu"));
-        //enterButton1.click();
+        WebElement enterButton1 = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/ul/li[3]/a"));
+        enterButton1.click();
+        WebElement enterButton2 = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/ul/li[3]/ul"));
+        enterButton2.click();
     }
 }
