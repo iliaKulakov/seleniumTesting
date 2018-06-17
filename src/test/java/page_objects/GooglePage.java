e929a1f9b2a2a13e814cb6e7173d1be2f9bebf4c
Rinat Muhamedgaliev <rinat.muhamedgalie@gmail.com>
2018-06-17 22:14:47 +0300

    Clean code

--- src/test/java/page_objects/GooglePage.java
+++ src/test/java/page_objects/GooglePage.java
@@ -1,14 +1,8 @@
 package page_objects;
 
-import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
-public class GooglePage  {
+public class GooglePage {
 
 
     public static By searchLineLocator = By.xpath("//*[@id=\"lst-ib\"]");
---
+++ src/test/java/tests/GoogleSearchTest.java
@@ -0,0 +1,76 @@
+package tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import page_objects.GoogleImagePage;
+import page_objects.GooglePage;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+//@RunWith(Parameterized.class)
+public class GoogleSearchTest {
+
+    public static WebDriver driver;
+    private String errorMessage;
+
+    @Before
+    public void setUp() {
+        FileInputStream fis;
+        Properties properties = new Properties();
+        try {
+            fis = new FileInputStream("application.properties");
+            properties.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.setProperty("webdriver.chrome.driver", properties.getProperty("webdriver.path"));
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void goToGooglePage() {
+        driver.get("http://google.com");
+        GooglePage googlePage = new GooglePage();
+        try {
+            WebElement searchLine = driver.findElement(googlePage.searchLineLocator);
+        } catch (NoSuchElementException e) {
+        }
+    }
+
+
+    @Test
+    public void checkImageButton() {
+        driver.get("http://google.com");
+        GooglePage googlePage = new GooglePage();
+        WebElement googleImage = driver.findElement(googlePage.imageButton);
+        googleImage.click();
+        try {
+            GoogleImagePage googleImagePage = new GoogleImagePage();
+            WebElement imageSearchButton = driver.findElement(googleImagePage.imageSearchButton);
+            imageSearchButton.click();
+            WebElement loadImageButton = driver.findElement(googleImagePage.loadImageButton);
+            loadImageButton.click();
+            WebElement closeSearchWindowButton = driver.findElement(googleImagePage.closeSearchWindowButton);
+            closeSearchWindowButton.click();
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
+
+
