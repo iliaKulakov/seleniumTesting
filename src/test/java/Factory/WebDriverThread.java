b93b649e6490a63621781b4fcc8acd45340d7130
ilia <38052676+iliaKulakov@users.noreply.github.com>
2018-06-21 07:49:30 +0300

    Убрал неиспользуемый класс Нужно исправить класс - WebDriverThread - там у меня некорректная реализация метода GetDriver

--- src/test/java/Factory/WebDriverThread.java
+++ src/test/java/Factory/WebDriverThread.java
@@ -1,12 +1,39 @@
 package Factory;
 
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class WebDriverThread {
 
     private WebDriver webdriver;
+
+
+    @Before
+    public void setUp() {
+        InputStream fis;
+        Properties properties = new Properties();
+        try {
+            fis = this.getClass().getClassLoader().getResourceAsStream("application.properties");
+            properties.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.setProperty("webdriver.chrome.driver", properties.getProperty("webdriver.path"));
+        webdriver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown() {
+        webdriver.quit();
+    }
+
 
     private final String operatingSystem =
             System.getProperty("os.name").toUpperCase();
@@ -23,7 +50,7 @@ public class WebDriverThread {
             System.out.println("Current Browser Selection: Chrome");
                     System.out.println(" ");
             webdriver = new
-                    ChromeDriver(DesiredCapabilities.chrome()); //странно, но ок для примера пока нужно
+                    ChromeDriver(DesiredCapabilities.chrome());
         }
         return webdriver;
     }
@@ -35,4 +62,9 @@ public class WebDriverThread {
             }
         }
 
+    public static void main (String args []) throws Exception {
+        WebDriverThread webDriverThread = new WebDriverThread();
+
+        webDriverThread.getDriver();
+    }
 }
